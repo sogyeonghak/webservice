db5d63929cbb555a1135cfd94b65ce739b184f26
sogyeonghak <78330852+sogyeonghak@users.noreply.github.com>
2021-12-12 13:26:28 +0900

    12/12

---
+++ src/main/java/com/sogang/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.sogang.book.springboot.web.dto;
+
+
+import com.sogang.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
